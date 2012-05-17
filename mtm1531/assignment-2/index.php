f7d82987c29eb09b0dae20315b11372a34a65785
soudaou <souaddaou@gmail.com>
2012-05-17 19:45:05 -0400

    Moved the php code to the top, was not working when it was under the <form>. The php code with the variables needed to be executred first.

--- mtm1531/assignment-2/index.php
+++ mtm1531/assignment-2/index.php
@@ -1,36 +1,4 @@
-<?php
-var_dump($_POST);
-?><!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8">
-	<title>Money calculator form</title>
-	<link href="css/general.css" rel="stylesheet">
-</head>
-
-<body>
-	<?php if ($_SERVER['REQUEST_METHOD'] == 'GET') : ?>
-	<form method="post" action="index.php">
-		<label for="number1">Number 1</label>
-		<input type="number"input id="number1" name="number1">
-		
-		<label for="number2">Number 2</label>
-		<input type="number" input id="number2" name="number2">
-		
-		<label for="function">Function</label>
-		<select id="function" name="function">
-			<option value="+">+</option>
-			<option value="-">-</option>
-			<option value="/">/</option>
-			<option value="*">*</option>
-		</select>
-		
-		<button>Calculate</button>
-		
-		<strong>$<?php echo number_format($total, 2); ?></strong>
-	</form>
-			<?php
-			
+<?php			
 $number1 = 0;
 $number2 = 0;
 $function = '+';
@@ -64,7 +32,36 @@ if (isset($_POST['function'])) {
 }
 	$total = $answer * 1.13;
 
-	?>
+?><!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+	<title>Money calculator form</title>
+	<link href="css/general.css" rel="stylesheet">
+</head>
+
+<body>
+	<?php if ($_SERVER['REQUEST_METHOD'] == 'GET') : ?>
+	<form method="post" action="index.php">
+		<label for="number1">Number 1</label>
+		<input type="number"input id="number1" name="number1">
+		
+		<label for="number2">Number 2</label>
+		<input type="number" input id="number2" name="number2">
+		
+		<label for="function">Function</label>
+		<select id="function" name="function">
+			<option value="+">+</option>
+			<option value="-">-</option>
+			<option value="/">/</option>
+			<option value="*">*</option>
+		</select>
+		
+		<button>Calculate</button>
+		
+		<strong>$<?php echo number_format($total, 2); ?></strong>
+	</form>
+
 
 </body>
 </html>
