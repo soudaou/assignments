ec43bc242327caeaf685a5c69d0b9fc9d43586c2
soudaou <souaddaou@gmail.com>
2012-05-24 15:22:28 -0400

    created a new variable called newitem and added it to the <li> when clicking on the add button

--- mtm1526/assignment-2/js/to-do.js
+++ mtm1526/assignment-2/js/to-do.js
@@ -5,3 +5,12 @@ var myform = document.getElementById('myform');
 //added item to list
 myform.addEventListener('submit', function (ev) {
 	ev.preventDefault();
+	
+	if (additem.value != '') {
+		var newItem = document.createElement('li');
+		newItem.innerHTML = additem.value;
+		todolist.appendChild(newItem);
+	}
+
+	additem.value = '';
+}, false);
