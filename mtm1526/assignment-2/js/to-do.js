b79776d80b37389563cb39cc295fe239064917e4
soudaou <souaddaou@gmail.com>
2012-05-24 15:21:18 -0400

    added the eventlistener to the myform

--- mtm1526/assignment-2/js/to-do.js
+++ mtm1526/assignment-2/js/to-do.js
@@ -2,3 +2,6 @@ var additem = document.getElementById('additem');
 var todolist = document.getElementById('todolist');
 var myform = document.getElementById('myform');
 
+//added item to list
+myform.addEventListener('submit', function (ev) {
+	ev.preventDefault();
