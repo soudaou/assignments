c2536040af098e0ec5b8d21efd2ecbc8f8679c25
soudaou <souaddaou@gmail.com>
2012-05-25 08:57:58 -0400

    added the link to the css file and added a form tag indie the body tag

--- mtm1531/assignment-3/index.php
+++ mtm1531/assignment-3/index.php
@@ -2,9 +2,16 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+	<title>Registration Form Validation</title>
+	<link href="css/general.css" rel="stylesheet">
 </head>
 
 <body>
+
+	<form method="post" action="index.php">
+    
+    </form>
+
+
 </body>
 </html>
