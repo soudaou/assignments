77f1524ace7f8a0628e7f46ce99b92bb22cda957
soudaou <souaddaou@gmail.com>
2012-05-25 09:43:04 -0400

    took off some comments out

--- mtm1531/assignment-3/includes/form-processor.php
+++ mtm1531/assignment-3/includes/form-processor.php
@@ -1,10 +1,23 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
+$errors = array();
 
-<body>
-</body>
-</html>
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+
+
+/*Validation*/
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+//to validate the name
+	if (empty($name)) {
+		$errors['name'] = true;
+	}
+//to validate the email
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+		$errors['email'] = true;
+	}
+
+
+
+
+}
