b2edcf2f77732746ce4559aaa460fdd809362567
soudaou <souaddaou@gmail.com>
2012-05-25 10:40:41 -0400

    added a new variable called username

--- mtm1531/assignment-3/includes/form-processor.php
+++ mtm1531/assignment-3/includes/form-processor.php
@@ -3,20 +3,26 @@ $errors = array();
 
 $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$username = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
 
 
 /*Validation*/
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-//to validate the name
+
+	//to validate the name
 	if (empty($name)) {
 		$errors['name'] = true;
 	}
-//to validate the email
+	//to validate the email
 	if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
 		$errors['email'] = true;
 	}
 
+//to validate the username
+	if (empty($username)) {
+		$errors['username'] = true;
+	}
 
 
 
