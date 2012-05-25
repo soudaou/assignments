8b55e3f8378e47e831cdef135558cd3f777a6fed
soudaou <souaddaou@gmail.com>
2012-05-25 11:04:18 -0400

    added the variables for the notes and terms

--- mtm1531/assignment-3/includes/form-processor.php
+++ mtm1531/assignment-3/includes/form-processor.php
@@ -5,7 +5,8 @@ $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, 'password', FILTER_UNSAFE_RAW);
-
+$lang = filter_input(INPUT_POST, 'lang', FILTER_SANITIZE_STRING);
+$notes = filter_input(INPUT_POST, 'notes', FILTER_SANITIZE_STRING);
 
 /*Validation*/
 
@@ -24,7 +25,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 	if (empty($username)) {
 		$errors['username'] = true;
 	}
-
-
-
+	
+//to validate the radio
+if ($lang != 'spanish' || $lang != 'french' || $lang != 'english'){
+		$errors['lang'] = true;
+	}
+	
+//to validate the message
+	if (mb_strlen($notes) < 5 || mb_strlen($notes) > 100 ){
+		$errors['notes'] = true;
+	}
+	
+//to validate the terms
+	if (!isset($_POST['terms'])){
+		$errors['terms'] = true;
+	}
 }
