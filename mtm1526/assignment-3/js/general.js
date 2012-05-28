addf7adee113581fd982f3b1574ce8a58fe441c6
soudaou <souaddaou@gmail.com>
2012-05-28 18:07:48 -0400

    added $('form').on('submit', function (ev) { to the js file

--- mtm1526/assignment-3/js/general.js
+++ mtm1526/assignment-3/js/general.js
@@ -1,20 +1,34 @@
 $(document).ready(function() {
 	
-	var property;
-	var color;
-	
-	$('#bordercolor').on('click', function (ev) {
-		property = $('#bordercolor').val();
+	$('form').on('submit', function (ev) {
+		
+		ev.preventDefault();
+		var property;
+		var color = $('#color-box').val();
+		
+		$('#border-color').on('click', function (ev) {
+			property = $('#bordercolor').val();
+		});
+		$('#background-color').on('click', function (ev) {
+			property = $('#backgroundcolor').val();
+		});
+		$('#color').on('click', function (ev) {
+			property = $('#color').val();
+		});
+		
+		$('#apply').on('click', function() {
+			// we are refering to the li so we called it "this"
+			//$(this).toggleClass('done');
+		});
+		
+		$('#apply').on('click', function(typecolor) {
+		//(document.getElementById('color-box'), 'change', function() {
+			//Inside an event Listener
+			//this refers to the element this listener was bound to
+			//circle.style.backgroundColor = this.value;
+			//you can add HTML to the value
+			acircle.innerHTML = '<strong>' + acircle.value + '</strong>';
+		});
 	});
-	$('#backgroundcolor').on('click', function (ev) {
-		property = $('#backgroundcolor').val();
-	});
-	$('#color').on('click', function (ev) {
-		property = $('#color').val();
-	});
-	
-	$('#apply').on('click', function() {
-		// we are refering to the li so we called it "this"
-		//$(this).toggleClass('done');
-	});
+
 });
