98c8c3ae2b37261054a391c3a51fd32b30f6b930
soudaou <souaddaou@gmail.com>
2012-05-28 17:41:34 -0400

    added the functions of the property

--- mtm1526/assignment-3/js/general.js
+++ mtm1526/assignment-3/js/general.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
 	
+	var property;
+	var color;
+	
+	$('#bordercolor').on('click', function (ev) {
+		property = $('#bordercolor').val();
+	});
+	$('#backgroundcolor').on('click', function (ev) {
+		property = $('#backgroundcolor').val();
+	});
+	$('#color').on('click', function (ev) {
+		property = $('#color').val();
+	});
+	
+	$('#apply').on('click', function() {
+		// we are refering to the li so we called it "this"
+		//$(this).toggleClass('done');
+	});
 });
