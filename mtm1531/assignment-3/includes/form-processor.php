233a508a15e434bb1763ca9e88ca2a427bcb3fce
soudaou <souaddaou@gmail.com>
2012-05-28 13:03:13 -0400

    had an error in the email and was not working

--- mtm1531/assignment-3/includes/form-processor.php
+++ mtm1531/assignment-3/includes/form-processor.php
@@ -10,7 +10,7 @@ $password = filter_input(INPUT_POST, 'password', FILTER_UNSAFE_RAW);
 $preferredlang = filter_input(INPUT_POST, 'preferredlang', FILTER_SANITIZE_STRING);
 $notes = filter_input(INPUT_POST, 'notes', FILTER_SANITIZE_STRING);
 $souad = 'Souad Daou';
-$email = 'daou0092@algonquinlive.com';
+$myemail = 'daou0092@algonquinlive.com';
 $subject = 'Registration form';
 $message = 'Thank you for registering';
 
@@ -29,20 +29,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 //to validate the username
 	if (strlen($username) < 1 || strlen($username) > 25)
-    $errors['username'] = true;
+    	$errors['username'] = true;
 	
-	
+//to validate the password
+	if (empty($password)) {
+   	 	$errors['password'] = true;
+	}
+		
 //to validate the language
 	if (!in_array($preferredlang, array('english', 'french', 'spanish')))
-   $errors['preferredlang'] = true;
-   //if ($preferredlang != 'english' || $preferredlang != 'french' || $preferredlang != 'spanish'){
-	//	$errors['preferredlang'] = true;
-	//}
-	
-//to validate the notes
-	if (mb_strlen($notes) < 5 || mb_strlen($notes) > 100 ){
-		$errors['notes'] = true;
-	}
+   		$errors['preferredlang'] = true;
 	
 //to validate the terms
 	if (!isset($_POST['terms'])){
@@ -50,11 +46,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 	}
 	
 //to validate the message
-	if (empty($errors)) {
-		echo 'Thank you for registering';
-    $thanks = true;
+
+
+if (empty($errors)){
+	$thanks = true;
 	$headers = 'From: ' . $souad . '<' . $email . '>';
-	mail($email, $message, $subject, $headers);
-  }
-  
+	mail('souaddaou@gmail.com', $subject, $message, $headers);
+	}
 }
