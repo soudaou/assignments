6427fcc740efcbfbb74cccbcd7bc33aa1c39a394
soudaou <souaddaou@gmail.com>
2012-05-28 22:49:58 -0400

    added my #hide-show to the js file

--- mtm1526/assignment-3/js/general.js
+++ mtm1526/assignment-3/js/general.js
@@ -1,13 +1,12 @@
-//var isHidden = false;
-
 $(document).ready(function() {
+	
+	var circleHidden = false;
+	var property = $('#property');
+	var color = $('#color-box').val();
 	
 	$('form').on('submit', function (ev) {
 		ev.preventDefault();
-		
-		var property;
-		var color = $('#color-box').val();
-		
+
 		switch ($('#property').val()){
 	case 'border-color':
 		$('.acircle').css('border-color', color);
@@ -19,9 +18,17 @@ $(document).ready(function() {
 		$('.acircle').css('color', color);
 		break;
 		}
-		$('#hide-show').on('click', function() {
-			
-		});
-	
 	});
-})
+		
+	$('#hide-show').on('click', function (ev) {
+		if (circleHidden == false) {
+			$('.acircle').hide().fadeOut(200);
+			circleHidden = true;
+		}
+		else{
+			$('.acircle').hide().fadeIn(100);
+			circleHidden = false;
+		}
+	});
+
+});
