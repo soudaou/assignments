804a0d3f5066e600cfbef5b828a2f2a258ab495b
soudaou <souaddaou@gmail.com>
2012-05-28 21:55:49 -0400

    found my little error. Had ' ' between around my color

--- mtm1526/assignment-3/js/general.js
+++ mtm1526/assignment-3/js/general.js
@@ -1,21 +1,28 @@
+//var isHidden = false;
+
 $(document).ready(function() {
+	console.log("ready play around");
 	
 	$('form').on('submit', function (ev) {
-		
 		ev.preventDefault();
+		
 		var property;
 		var color = $('#color-box').val();
 		
 		switch ($('#property').val()){
-	case '#border-color':
-		$('.acircle').css('border-color', 'color-box');
+	case 'border-color':
+		$('.acircle').css('border-color', color);
 		break;
-	case '#background-color':
-		$('.acircle').css('background-color', 'color-box');
+	case 'background-color':
+		$('.acircle').css('background-color', color);
 		break;
-	case '#color':
-		$('.acircle').css('color', 'color-box');
+	case 'color':
+		$('.acircle').css('color', color);
 		break;
 		}
+		$('#hide-show').on('click', function() {
+			
+		});
+	
 	});
 })
