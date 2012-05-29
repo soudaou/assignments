59e5ee7538703946429b500e521cd56e98dfac3b
soudaou <souaddaou@gmail.com>
2012-05-28 21:29:54 -0400

    deleted the code i had previously and replacing with switch statement for my property

--- mtm1526/assignment-3/js/general.js
+++ mtm1526/assignment-3/js/general.js
@@ -5,30 +5,7 @@ $(document).ready(function() {
 		ev.preventDefault();
 		var property;
 		var color = $('#color-box').val();
-		
-		$('#border-color').on('click', function (ev) {
-			property = $('#bordercolor').val();
-		});
-		$('#background-color').on('click', function (ev) {
-			property = $('#backgroundcolor').val();
-		});
-		$('#color').on('click', function (ev) {
-			property = $('#color').val();
-		});
-		
-		$('#apply').on('click', function() {
-			// we are refering to the li so we called it "this"
-			//$(this).toggleClass('done');
-		});
-		
-		$('#apply').on('click', function(typecolor) {
-		//(document.getElementById('color-box'), 'change', function() {
-			//Inside an event Listener
-			//this refers to the element this listener was bound to
-			//circle.style.backgroundColor = this.value;
-			//you can add HTML to the value
-			acircle.innerHTML = '<strong>' + acircle.value + '</strong>';
-		});
-	});
 
-});
+		
+	});
+})
