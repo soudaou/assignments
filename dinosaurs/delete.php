9541a46d8587caaf1f4ffa5c7ff10d29cb24ba34
soudaou <souaddaou@gmail.com>
2012-06-04 14:48:11 -0400

    created a delete.php file

---
+++ dinosaurs/delete.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'includes/db.php';
+
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+if (empty($id)) {
+	header('Location: index.php');
+	exit;
+}
+
+$sql = $db->prepare('
+	DELETE FROM dinosaurs
+	WHERE id = :id
+');
+
+$sql->bindValue(':id', $id, PDO::PARAM_INT);
+$sql->execute();
+
+// After the dino has been permanently deleted the user will be redirected by to index.php
+header('Location: index.php');
+exit;
