3180db86d8441ef2a9c34e3be2cfd9d6164b9cbf
soudaou <souaddaou@gmail.com>
2012-06-04 15:28:14 -0400

    added the tags <h2>,<dl>,<dd> and<dt> the the php code

--- mtm1531/assignment-5/index.php
+++ mtm1531/assignment-5/index.php
@@ -1,10 +1,25 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
+	<meta charset="utf-8">
+	<title>Movies</title>
 </head>
 
 <body>
+
+	<?php foreach ($results as $movie) : ?>
+	<h2>
+		<a href="single.php?id=<?php echo $movie['id']; ?>">
+		<?php echo $movie['movie-title']; ?>
+		</a>
+	</h2>
+	<dl>
+		<dt>Release Date</dt>
+		<dd><?php echo $movie['release_date']; ?></dd>
+		<dt>Director</dt>
+		<dd><?php echo $movie['director']; ?></dd>
+	</dl>
+	<?php endforeach ?>
+	
 </body>
 </html>
