e5a290b317743b9f572d2d746c42c14c19435586
soudaou <souaddaou@gmail.com>
2012-06-04 14:49:17 -0400

    added php comments to the tags

--- dinosaurs/index.php
+++ dinosaurs/index.php
@@ -1,4 +1,20 @@
-<!DOCTYPE HTML>
+<?php
+
+require_once 'includes/db.php';
+
+$sql = $db->query('
+	SELECT id, dino_name, loves_meat, in_jurassic_park
+	FROM dinosaurs
+	ORDER BY dino_name ASC
+');
+// This will display the last error created by our database
+// Should get rid of it as soonas you find your error
+//var_dump($db->errorInfo());
+
+$results = $sql->fetchALL();
+// The dino variable is for each iteration
+
+?><!DOCTYPE HTML>
 <html>
 <head>
 	<meta charset="utf-8">
@@ -6,14 +22,19 @@
 </head>
 
 <body>
-
-	<h2>Velociraptor</h2>
+	<?php foreach ($results as $dino) : ?>
+	<h2>
+		<a href="single.php?id=<?php echo $dino['id']; ?>">
+		<?php echo $dino['dino_name']; ?>
+		</a>
+	</h2>
 	<dl>
-		<dt>Loves Meat</dt>
-		<dd>1</dd>
+		<dt>Loves meat</dt>
+		<dd><?php echo $dino['loves_meat']; ?></dd>
 		<dt>In Jurassic Park</dt>
-		<dd>1</dd>
+		<dd><?php echo $dino['in_jurassic_park']; ?></dd>
 	</dl>
+	<?php endforeach ?>
 
 </body>
 </html>
