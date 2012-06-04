88846dcdaf6a36c4185db75957c5da2e98df1383
soudaou <souaddaou@gmail.com>
2012-06-04 15:44:31 -0400

    added the require link to db.php
    added an if statement if movie is deleted go back to index.php

--- mtm1531/assignment-5/delete.php
+++ mtm1531/assignment-5/delete.php
@@ -1,10 +1,21 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
 
-<body>
-</body>
-</html>
+require_once 'includes/db.php';
+
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+if (empty($id)) {
+	header('Location: index.php');
+	exit;
+}
+
+$sql = $db->prepare('
+	DELETE FROM dinosaurs
+	WHERE id = :id
+');
+
+$sql->bindValue(':id', $id, PDO::PARAM_INT);
+$sql->execute();
+
+header('Location: index.php');
+exit;
