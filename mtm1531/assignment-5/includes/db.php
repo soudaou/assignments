ada14c8da33cedc3f04584f5cf6d5b92065cb2b8
soudaou <souaddaou@gmail.com>
2012-06-04 16:01:25 -0400

    removed some extra spaces

--- mtm1531/assignment-5/includes/db.php
+++ mtm1531/assignment-5/includes/db.php
@@ -1,8 +1,10 @@
 <?php
 
-$user = getenv('DB_USER');	
-$pass = getenv('DB_PASS');	
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
 
 $data_source = getenv('DATA_SOURCE');
-$db = new PDO($data_source, $user, $pass);
+
+$db = new PDO ($data_source, $user, $pass);
+
 $db->exec('SET NAMES utf8');
