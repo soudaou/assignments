d6e2f182fed9fe6914aa41abce4949c28cbdf8bd
soudaou <souaddoau@gmail.com>
2012-06-11 12:41:32 -0400

    added html form for the user to use to add dinosaurs

--- dinosaurs/add.php
+++ dinosaurs/add.php
@@ -2,11 +2,36 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Add a Dinosaur</title>
+<title>Add New Dinosaur</title>
 </head>
 
 <body>
-
+	
+	<h1>Add a New Dinosaur</h1>
+	
+	<form method="post" actions="add.php">
+		<div>
+			<label for="dino_name"> Dinosaur Name </label>
+			<input id="dino_name" name="dino_name" required>
+		</div>
+		
+		<fieldset>
+			<legend> Relationship with meat </legend>
+			<input type="radio" id="love" name="loves_meat" value="1">
+			<label for="love"> Loves Meat </label>
+			
+			<input type="radio" id="hate" name="loves_meat" value="0">
+			<label for="hate"> Hates Meat </label>
+		</fieldset>
+		
+		<div>
+			<input type="checkboc" id="in_jurassic_park" name="loves_meat">
+			<label for="in_jurassic_park"> In jUrassic Park? </label>
+		</div>
+		
+		<button type="submit"> Add</button>
+		
+	</form>
 
 
 </body>
