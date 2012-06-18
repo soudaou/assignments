99fa94e4858718558bb689c37cd08e9198c1a4d3
souadaou <soauddaou@gmail.com>
2012-06-18 14:18:33 -0400

    added ul

---
+++ mtm1526/assignment-4/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>my cupcake</title>
+	<link href="css/general.css" rel="stylesheet">
+</head>
+
+<body>
+	<nav>
+		<ul id="cupcakes" >
+				<li><a href="coconut-cupcake.html" class="selected" rel="#default">Coconut</a></li>
+				<li><a href="vanilla-cupcake.html" rel="">Vanilla</a></li>
+				<li><a href="raspberry-cupcake.html" rel="">Raspberry</a></li>
+</ul>
+	</nav>
+
+</body>
+</html>
