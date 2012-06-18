35913a932e01f6c5be3835f078227c9e8c1b7aaf
souadaou <soauddaou@gmail.com>
2012-06-18 16:07:00 -0400

    added an empty div with an id

--- mtm1526/assignment-4/index.php
+++ mtm1526/assignment-4/index.php
@@ -9,12 +9,16 @@
 <body>
 	<nav>
 		<ul id="cupcakes" >
-				<li><a href="coconut.html" class="selected" rel="#default">Coconut</a></li>
-				<li><a href="vanilla.html" rel="">Vanilla</a></li>
-				<li><a href="raspberry.html" rel="">Raspberry</a></li>
-				<li><a href="lemon.html" rel="">Raspberry</a></li>
+				<li><a href="coconut.html" class="current" rel="#default"><strong>Coconut</strong></a></li>
+				<li><a href="vanilla.html" rel=""><strong>Vanilla</strong></a></li>
+				<li><a href="lemon.html" rel=""><strong>Raspberry</strong></a></li>
+				<li><a href="raspberry.html" rel=""><strong>Lemon</strong></a></li>
 		</ul>
 	</nav>
+	
+	<div id="fillme"></div>
 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="js/my-cupcake.js"></script>
 </body>
 </html>
