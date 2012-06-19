b554d79ca64bf285663db44db2cc95d4838f32fb
soudaou <souaddaou@gmail.com>
2012-06-19 17:55:14 -0400

    called the id for the tabs to open each page

--- mtm1526/assignment-4/js/my-cupcake.js
+++ mtm1526/assignment-4/js/my-cupcake.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
 	
 	$.get('coconut.html', function (data) {
-		$('body').prepend(data);
+		$('#fillme').append(data);
 	});
 
+	$('#coconut').on('click', function () {
+		$('#fillme').load('coconut.html');
+	});
+	$('#vanilla').on('click', function () {
+		$('#fillme').load('vanilla.html');
+	});
+	$('#raspberry').on('click', function () {
+		$('#fillme').load('raspberry.html');
+	});
+	$('#lemon').on('click', function () {
+		$('#fillme').load('lemon.html');
+	});
+	
 });
