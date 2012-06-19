c30c92cd5e24105639bbd513409faead6d7b9c22
soudaou <souaddaou@gmail.com>
2012-06-19 12:52:15 -0400

    having some error somewhere, still lookig for it

--- dinosaurs/includes/users.php
+++ dinosaurs/includes/users.php
@@ -22,8 +22,54 @@ function user_create ($db, $username, $password) {
 }
 
 function user_is_signed_in() {
-	if(!isset($_SESSION['user-id'] )) {
+	if(
+		!isset($_SESSION['user-id'] )
+		|| empty($_SESSION['user-id'])
+		|| !isset($_SESSION['user-id'])
+		|| $_SESSION['fingerprint'] == get_fingerprint($_SESSION['user-id'])
+	) {
 		return false;
 	}
 	return true;
+}
+
+function user_get ($db, $username, $password){
+	$sql = $db->prepare('
+		SELECT id, username, password
+		FROM users
+		WHERE username = :username
+		LIMIT 1
+	');
+	$sql->bindValue(':username', $username, PDO::PARAM_STR);
+	$sql->execute();
+	$user = $sql->fetch();
+	
+	if (empty($user) || !password_match($password, $user['password'])) {
+		return false;
+	}
+		return $user['id'];	
+}
+
+function password_match($pass_clear_text, $pass_hashed) {
+	return crypt($pass_clear_text, $pass_hashed) == $pass_hashed;
+}
+
+function user_sign_in ($user_id) {
+	session_regenerate_id();
+	$_SESSION['user_id'] = $user_id;
+	$_SESSION['fingerprint'] = get_fingerprint($user_id);
+}
+
+function get_fingerprint ($user_id) {
+	return sha1(
+		$user_id 
+		. $_SERVER['REMOTE_ADDR']
+		. session_id()
+		. $_SERVER['HTTP_USER_AGENT']
+	);
+}
+//this is how you sign the user out
+function user_sign_out(){
+	$_SESSION = array();
+	session_destroy();	
 }
