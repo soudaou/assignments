b91214a99bfe6fe4e90ef4314cee4dad60d9c590
soudaou <souaddaou@gmail.com>
2012-06-20 18:44:12 -0400

    added the a variable and added three line of code to call the current to open with each page

--- mtm1526/assignment-4/js/my-cupcake.js
+++ mtm1526/assignment-4/js/my-cupcake.js
@@ -1,22 +1,17 @@
 $(document).ready(function () {
+
+	var cupcakesTabs;
 	
 	$.get('coconut.html', function (data) {
 		$('#fillme').append(data);
 	});
 	
-	if($(cupcakes + ' LI.current A').length > 0){
-		loadTab($(cupcakes + ' LI.current A'));
-	}
 	
-	 $(cupcakes + ' A').click(function(){
-    	if($(this).parent().hasClass('current')){ return false; }
-    	
-    	$(cupcakes + ' LI.current').removeClass('current');
-    	$(this).parent().addClass('current');
-    	
-    	loadTab($(this));    	
-        return false;
-    });
+	$('a').on('click', function (event) {
+		cupcakesTabs= event.target.id;
+		$('ul' + ' li.current').removeClass('current');
+		$(this).parent().addClass('current');
+	});
 	
 	$('#coconut').on('click', function () {
 		$('#fillme').load('coconut.html');
