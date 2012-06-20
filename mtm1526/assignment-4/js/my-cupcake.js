666bb41a1f79596806eac468c870ce2a95dd3f23
soudaou <souaddaou@gmail.com>
2012-06-19 22:35:21 -0400

    added if statement for current

--- mtm1526/assignment-4/js/my-cupcake.js
+++ mtm1526/assignment-4/js/my-cupcake.js
@@ -3,7 +3,21 @@ $(document).ready(function () {
 	$.get('coconut.html', function (data) {
 		$('#fillme').append(data);
 	});
-
+	
+	if($(cupcakes + ' LI.current A').length > 0){
+		loadTab($(cupcakes + ' LI.current A'));
+	}
+	
+	 $(cupcakes + ' A').click(function(){
+    	if($(this).parent().hasClass('current')){ return false; }
+    	
+    	$(cupcakes + ' LI.current').removeClass('current');
+    	$(this).parent().addClass('current');
+    	
+    	loadTab($(this));    	
+        return false;
+    });
+	
 	$('#coconut').on('click', function () {
 		$('#fillme').load('coconut.html');
 	});
