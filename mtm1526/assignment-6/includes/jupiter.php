c7b632e0363f95c5b02a727d2ddefadac1e2d31c
soudaou <souaddaou@gmail.com>
2012-06-29 11:37:08 -0400

    added a jupiter.php

---
+++ mtm1526/assignment-6/includes/jupiter.php
@@ -0,0 +1,4 @@
+<h2>Jupiter</h2>
+
+<img src="images/jupiter.jpg" alt"">
+<p>Jupiter is the fifth planet from the Sun and the largest planet within the Solar System.[13] It is a gas giant with mass one-thousandth that of the Sun but is two and a half times the mass of all the other planets in our Solar System combined. Jupiter is classified as a gas giant along with Saturn, Uranus and Neptune. Together, these four planets are sometimes referred to as the Jovian or outer planets.</p>
