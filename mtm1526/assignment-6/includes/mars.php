6ebd34eacc762066ebd7814554e599c8fa793030
soudaou <souaddaou@gmail.com>
2012-06-29 11:37:31 -0400

    added mars.php

---
+++ mtm1526/assignment-6/includes/mars.php
@@ -0,0 +1,4 @@
+<h2>Mars</h2>
+
+<img src="images/mars.jpg" alt"">
+<p>Mars is the fourth planet from the Sun in the Solar System. Named after the Roman god of war, Mars, it is often described as the "Red Planet" as the iron oxide prevalent on its surface gives it a reddish appearance.[13] Mars is a terrestrial planet with a thin atmosphere, having surface features reminiscent both of the impact craters of the Moon and the volcanoes, valleys, deserts, and polar ice caps of Earth. </p>
