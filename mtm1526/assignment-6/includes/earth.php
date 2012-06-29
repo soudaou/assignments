f484dbcbeb2ce00556749b68efe83ae52dde9108
soudaou <souaddaou@gmail.com>
2012-06-29 11:36:08 -0400

    added earth.php

---
+++ mtm1526/assignment-6/includes/earth.php
@@ -0,0 +1,4 @@
+<h2>Earth</h2>
+
+<img src="images/earth.jpg" alt"">
+<p>Earth (or the Earth) is the third planet from the Sun, and the densest and fifth-largest of the eight planets in the Solar System.</p>
