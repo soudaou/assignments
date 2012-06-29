e9b776131d15a20fc2def23bff4bd95db31c5886
soudaou <souaddaou@gmail.com>
2012-06-29 11:38:26 -0400

    added venus.php

---
+++ mtm1526/assignment-6/includes/venus.php
@@ -0,0 +1,4 @@
+<h2>Venus</h2>
+
+<img src="images/venus.jpg" alt"">
+<p>Venus is the second planet from the Sun, orbiting it every 224.7 Earth days.[10] The planet is named after Venus, the Roman goddess of love and beauty. After the Moon, it is the brightest natural object in the night sky, reaching an apparent magnitude of −4.6, bright enough to cast shadows</p>
